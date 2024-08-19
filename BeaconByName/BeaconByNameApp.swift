1ccf20171adc0e734f0ef631b544083b520cc3b5
Frank Engelman <faengelm@gmail.com>
2024-08-19 08:41:33 -0700

    Initial Commit

---
+++ BeaconByName/BeaconByNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconByNameApp.swift
+//  BeaconByName
+//
+//  Created by Frank Engelman on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconByNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
